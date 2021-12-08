32d431677e5d094366de78641c6bd2872352e6cc
Razvan Rauta <rrauta@klika-tech.com>
2021-12-08 15:14:03 +0300

    vercel: fix middleware

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -12,20 +12,24 @@ export async function middleware(req: NextApiRequest) {
   const pathname = req.url;
 
   // let public urls pass through
-  if (
-    pathname
-    // process.env.PUBLIC_URLS.split('|').find((val) => pathname.startsWith(val))
-  ) {
-    // eslint-disable-next-line no-console
-    console.log({
-      url: process.env.PUBLIC_URLS.split('|').find((val) =>
-        pathname.startsWith(val)
-      ),
-      public: process.env.PUBLIC_URLS,
-      req: req.url,
-    });
-    return NextResponse.next();
+  if (process.env.NODE_ENV !== 'production') {
+    if (
+      pathname &&
+      process.env.PUBLIC_URLS.split('|').find((val) => pathname.startsWith(val))
+    ) {
+      return NextResponse.next();
+    }
+  } else {
+    if (
+      pathname &&
+      process.env.PUBLIC_URLS.split('|').find((val) =>
+        pathname.startsWith(`${process.env.NEXTAUTH_URL}${val}`)
+      )
+    ) {
+      return NextResponse.next();
+    }
   }
+
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   // redirect to login page if no session
